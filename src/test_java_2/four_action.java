73d74e117ce278409bf2c94745fbde69fa2d7cc6
ye <ye@DESKTOP-JAQ65G0>
2020-04-13 08:29:39 +0900

    0413_3

--- src/test_java_2/four_action.java
+++ src/test_java_2/four_action.java
@@ -15,14 +15,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/four_action")
 public class four_action extends HttpServlet{ 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
  
         response.setCharacterEncoding("UTF-8"); 
         request.setCharacterEncoding("UTF-8");
-        
 
+
+        // 자바스크립트에서 JSON 으로 보낸 값 사용방법 모름
+        
+        
 		String currentPage = request.getParameter("currentPage");
 		String countPerPage = request.getParameter("countPerPage");
 		String keyword = request.getParameter("keyword"); 
@@ -30,16 +32,11 @@ public class four_action extends HttpServlet{
 		String confmKey  = "U01TX0FVVEgyMDIwMDMyNjAxNTMxNjEwOTU4NTc=";
 		String resultType = "json";
 		
+		 
+		String api_url = "http://www.juso.go.kr/addrlink/addrLinkApi.do?currentPage="+currentPage
+				+"&countPerPage="+countPerPage+"&keyword="+keyword+
+				"&confmKey="+confmKey+"&resultType="+resultType;
 		
-		
-		
-		String api_url ="http://www.juso.go.kr/addrlink/addrLinkApi.do?currentPage=1&countPerPage=1&keyword=\"강서구7길\"\r\n" + 
-		"&confmKey=U01TX0FVVEgyMDIwMDMyNjAxNTMxNjEwOTU4NTc=&resultType=json\r\n";
-		
-//		String api_url = "http://www.juso.go.kr/addrlink/addrLinkApi.do?currentPage="+currentPage
-//				+"&countPerPage="+countPerPage+"&keyword="+keyword+
-//				"&confmKey="+confmKey+"&resultType="+resultType;
-//		
 		URL url = new URL(api_url);
 		BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));	
@@ -62,14 +59,9 @@ public class four_action extends HttpServlet{
 		
 		br.close();
 		bw.close();
-		response.setCharacterEncoding("UTF-8");
-//		response.setContentType("text/xml");
-//		response.getWriter().write(sb.toString()); 
-		
 		
 		request.setAttribute("api_data", sb.toString());
-		//RequestDispatcher dispatcher = .getRequestDispatcher("/four.html"); 
-		
+		 
 	}
 	
 	
